21ca8e7b1a7735dab491d922df81845f60306bf0
HENILBHUVA <henil.bhuva500@gmail.com>
2025-08-21 16:49:53 +0530

    first commit

---
+++ Backend/src/app.js
@@ -0,0 +1,39 @@
+import express from "express";
+import {createServer} from "node:http";
+
+import {Server} from "socket.io";
+
+import mongoose from "mongoose";
+
+import cors from "cors";
+import { connect } from "node:http2";
+import {connectToSocket} from "./controllers/socketManager.js";
+import userRoutes from "./routes/users.routes.js";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port",(process.env.PORT || 8000))
+app.use(cors());
+app.use(express.json({limit:"40kb"}));
+app.use(express.urlencoded({limit:"40kb", extended:true}));
+
+app.use("/api/v1/users", userRoutes);
+// app.use("/api/v2/users", newUserRoutes)
+
+const start = async ()=>{
+    app.set("mongo_user")
+    const connectionDb = await mongoose.connect("mongodb+srv://henilbhuva501:Bhuva%40500@cluster0.g88fe.mongodb.net/mydatabase?retryWrites=true&w=majority")
+
+    console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`)
+    server.listen(app.get("port"), ()=>{
+        console.log("LISTENING ON PORT 8000")
+    });
+
+}
+
+
+
+
+start();
